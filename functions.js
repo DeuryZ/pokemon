956640774f4a495fa95b5b3844299941a06b6ddd
DeuryZ <119361476+DeuryZ@users.noreply.github.com>
2023-10-01 10:24:13 -0500

    feat: :construction:

--- functions.js
+++ functions.js
@@ -1,3 +1,5 @@
+let pokedatos = [];
+
 export function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
@@ -6,10 +8,14 @@ export  async function randomPokemon(res, myContent) {
         let randomNum=getRandomInt(100);
         const element = res.results[randomNum];
         let poke = await (await fetch(`${element.url}`)).json();
+        let img = poke.sprites.front_default;
+        let defaultImg =
+        "https://i.pinimg.com/originals/27/ae/5f/27ae5f34f585523fc884c2d479731e16.gif";
+
         myContent.insertAdjacentHTML("beforeend", `
-        <div class="pokemonTypeContainer" pokeName="${element.name}" pokePhoto="${poke.sprites.front_default}">
-        <h1>${element.name}</h1>
-        <img src="${poke.sprites.front_default}" alt="">
+        <div class="pokemonTypeContainer" pokeName="${element.name}">
+        <h1 class="titlePokemon" pokeName="${element.name}">${element.name.toUpperCase()}</h1>
+        <img class="imgPokemon" pokeName="${element.name}" src="${img ? img : defaultImg}" alt="">
         <p>Type: ${poke.types[0].type.name}</p>
         </div>
         `);
@@ -31,4 +37,77 @@ export async function findTypePokemon(res){
             break;
         }
     }
+}
+let allTypesPokemon = [
+    'grass', 
+    'poison', 
+    'fire', 
+    'flying', 
+    'water', 
+    'bug', 
+    'normal', 
+    'electric', 
+    'ground', 
+    'fairy', 
+    'fighting', 
+    'psychic', 
+    'rock', 
+    'steel', 
+    'ice', 
+    'ghost', 
+    'dragon', 
+    'dark'
+];
+export async function showPokemon(e,select){
+    if (e.target.matches(select)){
+        let pokeName=e.target.getAttribute("pokeName")
+        let res = await (
+        await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)).json();
+        let img = res.sprites.front_default;
+        let defaultImg = "https://i.pinimg.com/originals/27/ae/5f/27ae5f34f585523fc884c2d479731e16.gif";
+        Swal.fire({
+        title: `${res.name.toUpperCase()}`,
+        text: "Modal with a custom image.",
+        imageUrl: `${img ? img : defaultImg}`,
+        html: `
+            ${res.stats.map(data => `<input type="range" class="range" value="${data.base_stat}" pokeStatName="${data.stat.name}" idName="${res.name}"><label><b idBase="${data.stat.name}">${data.base_stat}</b> ${data.stat.name.toUpperCase()} </label><br>`
+                ).join("")}
+            `,
+            
+        imageWidth: "80%",
+        imageHeight: "80%",
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: 'Save',
+        denyButtonText: `Don't save`,
+        }).then((result) => {
+        if (result.isConfirmed) {
+            saveData()
+        Swal.fire('Saved!', '', 'success')
+        } else if (result.isDenied) {
+            console.log("no save")
+        Swal.fire('Changes are not saved', '', 'info')
+        }
+    })
+    }
+    if (e.target.matches(".range")) {
+        const idName = e.target.getAttribute("idName");
+        const pokeStatName = e.target.getAttribute("pokeStatName");
+        const pokeStatValue = e.target.value;
+        const existingStat = pokedatos.find((stat) => stat.name === pokeStatName);
+            if (existingStat) {
+                existingStat.value = pokeStatValue;
+            } else {
+                pokedatos.push({ nameStat: pokeStatName, value: pokeStatValue, namePokemon: idName });
+            }
+            console.log(pokedatos);
+        }
+}
+async function saveData() {
+    let config = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(pokedatos)
+    }
+    let res = await(await fetch("https://650ad5b7dfd73d1fab08fcc0.mockapi.io/tabla/mokeapi", config)).json();
 }
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { randomPokemon } from "./functions.js";
+import { showPokemon } from "./functions.js";
 
 let myContent = document.querySelector(".content");
 let myContentSearch = document.querySelector(".contentSearch");
@@ -6,8 +7,26 @@ let myContentSearch = document.querySelector(".contentSearch");
 addEventListener("DOMContentLoaded", async() => {
     let res = await (await fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")).json();
     randomPokemon(res, myContent);
-    let allTypesPokemon = ['grass', 'poison', 'fire', 'flying', 'water', 'bug', 'normal', 'electric', 'ground', 'fairy', 'fighting', 'psychic', 'rock', 'steel', 'ice', 'ghost', 'dragon', 'dark'];
-
+    let allTypesPokemon = [
+        'grass', 
+        'poison', 
+        'fire', 
+        'flying', 
+        'water', 
+        'bug', 
+        'normal', 
+        'electric', 
+        'ground', 
+        'fairy', 
+        'fighting', 
+        'psychic', 
+        'rock', 
+        'steel', 
+        'ice', 
+        'ghost', 
+        'dragon', 
+        'dark'
+    ];
     let myTypes=document.querySelector(".types");
     allTypesPokemon.map(data=>{
         myTypes.insertAdjacentHTML("beforeend", `
@@ -21,12 +40,14 @@ addEventListener("DOMContentLoaded", async() => {
                 let res = await (await fetch(`https://pokeapi.co/api/v2/type/${e.target.getAttribute("idName")}`)).json();
                 myContent.style.display="none";
                 myContentSearch.innerHTML="";
+                let defaultImg =
+                "https://i.pinimg.com/originals/27/ae/5f/27ae5f34f585523fc884c2d479731e16.gif";
                 res.pokemon.map(async(data)=>{
                     let resImg=(await (await fetch(`${data.pokemon.url}`)).json())?(await (await fetch(`${data.pokemon.url}`)).json()):data.results[0].url;
                     myContentSearch.insertAdjacentHTML("beforeend", `
                     <div class="pokemonTypeContainer" pokeName="${data.pokemon.name}" pokePhoto="${resImg.sprites.front_default}">
-                    <h1>${data.pokemon.name.split('-').join(' ')}</h1>
-                    <img src="${resImg.sprites.front_default}" alt="">
+                    <h1 class="titlePokemon" pokeName="${data.pokemon.name}" >${data.pokemon.name.split('-').join(' ').toUpperCase()}</h1>
+                    <img class="imgPokemon"  pokeName="${data.pokemon.name}" src="${resImg.sprites.front_default ? resImg.sprites.front_default : defaultImg}" alt="">
                     </div>
                     `);
                 })
@@ -36,45 +57,8 @@ addEventListener("DOMContentLoaded", async() => {
 })
 
 document.addEventListener("click", async(e) => {
-    if (e.target.matches(".pokemonTypeContainer")){
-        let pokeName=e.target.getAttribute("pokeName")
-        let res = await (
-        await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
-        ).json();
-        let img = res.sprites.front_default;
-        let defaultImg =
-        "https://i.pinimg.com/originals/27/ae/5f/27ae5f34f585523fc884c2d479731e16.gif";
 
-        Swal.fire({
-        title: `${res.name}`,
-        text: "Modal with a custom image.",
-        imageUrl: `${img ? img : defaultImg}`,
-        html: `
-            ${res.stats.map(data => `<input type="range" class="range" value="${data.base_stat}"><label><b idBase="${data.stat.name}">${data.base_stat}</b> ${data.stat.name} </label><br>`
-                ).join("")}
-            `,
-        imageWidth: "80%",
-        imageHeight: "80%",
-        showDenyButton: true,
-        showCancelButton: true,
-        confirmButtonText: 'Save',
-        denyButtonText: `Don't save`,
-        }).then((result) => {
-        /* Read more about isConfirmed, isDenied below */
-        if (result.isConfirmed) {
-            let pokedatos = [];
-            if (e.target.matches(".range")){
-            console.log("save")
-            }
-            console.log(pokedatos)
-            localStorage.setItem(pokeName, JSON.stringify(pokedatos));
-        Swal.fire('Saved!', '', 'success')
-        } else if (result.isDenied) {
-            console.log("no save")
-        Swal.fire('Changes are not saved', '', 'info')
-        }
-    })
-    }
+    showPokemon(e, ".imgPokemon");
     if (e.target.matches(".range")){
         let val=e.target.value
         e.target.nextElementSibling.querySelector("b").textContent = val;
